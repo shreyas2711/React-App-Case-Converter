6a9512bc617437b67b9cf31b5e61094696f50cff
Shreyas <shreyas.kamath27@gmail.com>
2023-01-31 22:37:59 +0530

    Updates

---
+++ precache-manifest.2109ff7066f84030f9f77c868f2fe44c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2545fe124b64585994ec",
+    "url": "/React-App-Case-Converter/static/css/main.cd0347f9.chunk.css"
+  },
+  {
+    "revision": "2545fe124b64585994ec",
+    "url": "/React-App-Case-Converter/static/js/main.df5a6beb.chunk.js"
+  },
+  {
+    "revision": "f1b2ff9498a838122987",
+    "url": "/React-App-Case-Converter/static/js/runtime~main.abbecca4.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/React-App-Case-Converter/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/React-App-Case-Converter/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "ed74a426838e948dd79ce844a2286f25",
+    "url": "/React-App-Case-Converter/index.html"
+  }
+];
